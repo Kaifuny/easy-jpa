608b30670fd4076bf5d5d3c2417b6c5afb267e28
superbiger <490318637@qq.com>
2016-12-15 10:09:28 +0800

    add:原生specification支持

---
+++ src/main/java/com/sbiger/qbe/specification/custom/AndSpecification.java
@@ -0,0 +1,22 @@
+package com.sbiger.qbe.specification.custom;
+
+import com.sbiger.qbe.specification.AbstractSpecification;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.*;
+
+/**
+ * Created by sbiger on 2016-12-15.
+ */
+public class AndSpecification<T> extends AbstractSpecification<T> {
+    private Specification specification;
+
+    public AndSpecification(Specification specification) {
+        this.specification = specification;
+    }
+
+    @Override
+    public Predicate toPredicate(From root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+        return specification.toPredicate((Root) root, query, cb);
+    }
+}
